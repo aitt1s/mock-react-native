30b8952ebe168ea6ba5ec7d23ff6be9e6b1e9a3d
= <>
2018-09-16 12:15:51 +0300

    added flatlist

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,18 +1,19 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { StyleSheet, FlatList } from 'react-native'
 
 import ListItem from '../List/ListItem'
 
 const ListContainer = props => {
-  const placesOutput = props.places.map((place, i) => (
-    <ListItem 
-         key={i} 
-         placeName={place} 
-         onItemPressed={() => props.onItemDeleted(i)}
-    />
-  ))
-
-  return <View style={styles.listContainer}>{placesOutput}</View>
+  return <FlatList 
+          style={styles.listContainer}
+          data={props.places}
+          renderItem={(info) => (
+            <ListItem 
+              placeName={info.item.value} 
+              onItemPressed={() => props.onItemDeleted(info.item.key)}
+            />
+          )}
+        />
 }
 
 const styles = StyleSheet.create({
